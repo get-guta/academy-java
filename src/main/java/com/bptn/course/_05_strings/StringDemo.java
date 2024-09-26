2dd7b9e7e8eaaccf4aabe83c58d9a824a5c8d7f6
Getenesh Guta <76764412+get-guta@users.noreply.github.com>
2024-09-26 15:03:54 -0400

    update variable value

--- src/main/java/com/bptn/course/_05_strings/StringDemo.java
+++ src/main/java/com/bptn/course/_05_strings/StringDemo.java
@@ -5,12 +5,12 @@ public class StringDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String userName = " tastuser ";
+		String userName = " testuser ";
 		System.out.println(userName.length());
 		System.out.println(userName.charAt(2));
 		System.out.println(userName.substring(4));
 		System.out.println(userName.substring(2,3));
-		System.out.println(userName.equals("tastUser"));
+		System.out.println(userName.equals("testUser"));
 		System.out.println(userName.trim());
 		System.out.println(userName.indexOf('s'));
 
